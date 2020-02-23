a0f2810c6b33f87025bff0db33ad5cadb17cc948
Bowen Gao <bgao@caltech.edu>
2020-02-22 17:18:55 -0800

    print steps

--- examples/torchdiffeq/_impl/dopri5.py
+++ examples/torchdiffeq/_impl/dopri5.py
@@ -85,12 +85,15 @@ class Dopri5Solver(AdaptiveStepsizeODESolver):
     def advance(self, next_t):
         """Interpolate through the next time point, integrating as necessary."""
         n_steps = 0
+        lis=[]
+
         while next_t > self.rk_state.t1:
+            lis.append(self.rk_state.dt)
             assert n_steps < self.max_num_steps, 'max_num_steps exceeded ({}>={})'.format(n_steps, self.max_num_steps)
             self.rk_state = self._adaptive_dopri5_step(self.rk_state)
             n_steps += 1
 
-        return n_steps, _interp_evaluate(self.rk_state.interp_coeff, self.rk_state.t0, self.rk_state.t1, next_t)
+        return lis, _interp_evaluate(self.rk_state.interp_coeff, self.rk_state.t0, self.rk_state.t1, next_t)
 
     def _adaptive_dopri5_step(self, rk_state):
         """Take an adaptive Runge-Kutta step to integrate the ODE."""
