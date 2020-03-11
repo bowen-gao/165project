d6ddfe215244a5cb69a16e8191a74a6e3836db57
gaobowen <shixi_bowen16@staff.sina.com.cn>
2020-03-11 01:17:06 -0700

    add random select

--- examples/get_ids.py
+++ examples/get_ids.py
@@ -1,9 +1,22 @@
 import numpy as np
 
 allsteps = np.loadtxt('allsteps.txt')
-prev_ids = np.loadtxt("indices_mnist.txt", dtype=int)
 
-prev_ids = set(prev_ids)
 ind = np.argpartition(allsteps, -5000)[-5000:]
 
 np.savetxt("ids.txt", ind.astype(int), fmt='%i', delimiter=",")
+
+dic = {}
+
+for i in range(len(allsteps)):
+    dic[i] = allsteps[i]
+
+dic = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse=True)}
+
+tmp = []
+ids = []
+for i, k in enumerate(dic):
+    if i % 6000 < 500:
+        ids.append(k)
+ids=np.array(ids)
+np.savetxt("ids.txt", ids.astype(int), fmt='%i', delimiter=",")
